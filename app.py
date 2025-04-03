24349c4554cfc6a5c2d19db9c5ed6b819dea1746
ellt1988 <leonellee2016@gmail.com>
2025-04-03 12:26:08 -0600

    Modificaciones al archivo Python 03042025 1226

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ import pandas as pd
 df = pd.read_excel('final_champions_league.xlsx')
 
 def main():
-    st.title('Finales de Champions League')
-    st.header('Dataframe')
+    st.title('UEFA Champions League')
+    st.header('Histórico de finales Champions League')
+    st.subheader('Temporadas: de 1955 a 2023')
     st.dataframe(df)
 
 main()
