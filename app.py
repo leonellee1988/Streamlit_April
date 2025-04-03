980791e75e75ee39bf5dc0f3a3c21c5a50371c5b
ellt1988 <leonellee2016@gmail.com>
2025-04-03 13:27:33 -0600

    Modificaciones archivo Python 03042025 1327

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# Carga de paquetes:
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -12,20 +13,27 @@ def main():
     st.title('UEFA Champions League')
     st.header('Histórico de finales Champions League')
     st.subheader('Temporadas: de 1955 a 2023')
+
+    # Mostrar el dataframe en Streamlit:
     st.dataframe(df)
     
-    # Top 10 equipos más ganadores en Champions:
+    # Conteo de triunfos en finales por equipo o club:
     top_teams = df['winner'].value_counts().head(10)
 
     # Gráfica s/Top 10 equipos más ganadores en Champions:
-    sns.set(style="whitegrid")
-    colors = sns.color_palette("viridis", len(top_teams))
-    fig, ax = plt.subplots(figsize=(10, 6))
+    # Elegir estilo y paleta de colores Seaborn:
+    sns.set(style="whitegrid")                                                      
+    colors = sns.color_palette("viridis", len(top_teams))                           
+    fig, ax = plt.subplots(figsize=(10, 6))                                         
+    # Crear gráfico de barras:
     top_teams.plot(kind='bar', ax=ax, color=colors)
-    ax.set_title('Top 10 Equipos más Ganadores de la UEFA Champions League', fontsize=16, fontweight='bold')
+    ax.set_title('Top 10: Campeones de UEFA Champions League', fontsize=16, fontweight='bold')
+    # Configuración de titulos y etiquetas:
     ax.set_xlabel('Equipo', fontsize=12)
     ax.set_ylabel('Copas', fontsize=12)
+    # Rotación de las etiquetas del eje X:
     ax.set_xticklabels(ax.get_xticklabels(), rotation=45, ha='right')
+    # Mostrar el gráfico en Streamlit:
     st.pyplot(fig)
 
 main()
