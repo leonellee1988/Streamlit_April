191c6510f53d0a882dced03a632018cd91093983
ellt1988 <leonellee2016@gmail.com>
2025-04-03 12:32:44 -0600

    Cambios al archivo Python 03042025 1232

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
 
 df = pd.read_excel('final_champions_league.xlsx')
 
@@ -8,5 +9,15 @@ def main():
     st.header('Histórico de finales Champions League')
     st.subheader('Temporadas: de 1955 a 2023')
     st.dataframe(df)
+
+    top_teams = df['winner'].value_counts().head(10)
+
+    fig, ax = plt.subplots(figsize=(10, 6))
+    top_teams.plot(kind='bar', ax=ax, color='skyblue')
+    ax.set_title('Top 10 Equipos más Ganadores de la UEFA Champions League', fontsize=16)
+    ax.set_xlabel('Equipo', fontsize=12)
+    ax.set_ylabel('Número de Victorias', fontsize=12)
+    ax.set_xticklabels(ax.get_xticklabels(), rotation=45, ha='right')
+    st.pyplot(fig)
 
 main()
