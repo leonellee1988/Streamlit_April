0c6510aa497fa60046b6c10ef222c6af010523c4
ellt1988 <leonellee2016@gmail.com>
2025-04-03 11:57:23 -0600

    Cambios en archivo Python y agregado archivo CSV 03-04-2025 1157

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
 import streamlit as st
-import requests
-from bs4 import BeautifulSoup
 import pandas as pd
 
-url = 'https://es.uefa.com/uefachampionsleague/history/winners/finals/'
-response = requests.get(url)
-table = BeautifulSoup.find('table')
+df = pd.read_csv('ucl-finals.csv')
 
 def main():
     st.title('Finales de Champions League')
     st.header('Dataframe')
-    st.dataframe(table)
+    st.dataframe(df)
 
 main()
