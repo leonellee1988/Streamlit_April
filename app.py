c6ba17fd54397cad852ae4929b73ecfcb5f0328e
ellt1988 <leonellee2016@gmail.com>
2025-04-03 13:59:20 -0600

    Actualización logo 03042025 1359

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ df = pd.read_excel('final_champions_league.xlsx')
 def main():
 
     # Logo UEFA Champions League:
-    st.image('uefa-champions-league.jpg')
+    st.image('uefa-champions-league.png', width=500)
 
     # Encabezado de la App:
     st.title('UEFA Champions League')
