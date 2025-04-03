8ab4eb8493966297f53d591670010281bcd4b8c3
ellt1988 <leonellee2016@gmail.com>
2025-04-03 11:51:17 -0600

    Modificaciones archivo 1151

--- app.py
+++ app.py
@@ -1,21 +1,15 @@
 import streamlit as st
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+url = 'https://es.uefa.com/uefachampionsleague/history/winners/finals/'
+response = requests.get(url)
+table = BeautifulSoup.find('table')
 
 def main():
-    st.title('Curso de Streamlit')
-    st.header('Este es mi primer encabezado')
-    st.subheader('Este es mi primer sub-encabezado')
-    st.text('Hola mundo, estoy aprendiendo a crear Apps con Streamlit')
-
-    nombre = 'Edwin Lee'
-
-    st.text(f'Hola, mi nombre es {nombre}, y me encanta Python')
-    st.markdown('### Esto es un markdown')
-    st.success('Exito')
-    st.warning('Esto es una advertencia')
-    st.info('Esto es una información')
-    st.error('Error')
-
-    st.text('Esto es un texto normal')
-    st.text('### Pero también le puedo aplicar markdown')
+    st.title('Finales de Champions League')
+    st.header('Dataframe')
+    st.dataframe(table)
 
 main()
